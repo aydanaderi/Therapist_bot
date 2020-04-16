a7d6ee3e1925231043c5d3364fda13d9390336bf
ayda@ayda <ayda.f.naderi@gmail.com>
2020-04-16 17:12:21 +0430

    therapist_bot

---
+++ Therapist.py
@@ -0,0 +1,18 @@
+from nltk.chat.eliza import eliza_chatbot
+import  telepot
+import time
+
+bot = telepot.Bot('TOKEN')
+
+def talking(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    text = msg['text']
+    if text == "/start":
+        bot.sendMessage(chat_id, "Hello, I'm the therapist.  How can I help?")
+    else:
+        bot.sendMessage(chat_id, eliza_chatbot.respond(text))
+
+bot.message_loop(talking)
+print ('Listening ...')
+while 1:
+    time.sleep(10)
